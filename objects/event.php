38eca6c8dc22aaee93542262fa3031acba3eaadd
griffthegrouch <69768345+griffthegrouch@users.noreply.github.com>
2021-04-14 15:56:11 -0400

    added all CRUD functions for users table interaction

--- objects/event.php
+++ objects/event.php
@@ -116,43 +116,6 @@ class Event
         return false;
     }
 
-    // used when filling up the update event form
-    function readOne()
-    {
-
-        // query to read single record
-        $query = "SELECT
-                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
-            FROM
-                " . $this->table_name . " p
-                LEFT JOIN
-                    categories c
-                        ON p.category_id = c.id
-            WHERE
-                p.id = ?
-            LIMIT
-                0,1";
-
-        // prepare query statement
-        $stmt = $this->conn->prepare($query);
-
-        // bind id of event to be updated
-        $stmt->bindParam(1, $this->id);
-
-        // execute query
-        $stmt->execute();
-
-        // get retrieved row
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        // set values to object properties
-        $this->name = $row['name'];
-        $this->price = $row['price'];
-        $this->description = $row['description'];
-        $this->category_id = $row['category_id'];
-        $this->category_name = $row['category_name'];
-    }
-
     // update the event
     function update()
     {
@@ -305,45 +268,4 @@ class Event
         return $stmt;
     }
 
-
-    // read events with pagination
-    public function readPaging($from_record_num, $records_per_page)
-    {
-
-        // select query
-        $query = "SELECT
-                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
-            FROM
-                " . $this->table_name . " p
-                LEFT JOIN
-                    categories c
-                        ON p.category_id = c.id
-            ORDER BY p.created DESC
-            LIMIT ?, ?";
-
-        // prepare query statement
-        $stmt = $this->conn->prepare($query);
-
-        // bind variable values
-        $stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
-        $stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
-
-        // execute query
-        $stmt->execute();
-
-        // return values from database
-        return $stmt;
-    }
-
-    // used for paging events
-    public function count()
-    {
-        $query = "SELECT COUNT(*) as total_rows FROM " . $this->table_name . "";
-
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        return $row['total_rows'];
-    }
 }
--- objects/user.php
+++ objects/user.php
@@ -47,58 +47,37 @@ class User
         $query = "INSERT INTO
                 " . $this->table_name . "
             SET
-            noodleID= :noodleID, 
-            noodleTitle= :noodleTitle,
-            userID= :userID,
-            noodleStatus= :noodleStatus,
-            noodleDescription= :noodleDescription,
-            noodleTags= :noodleTags,
-            noodleImage= :noodleImage,
-            noodleLocation= :noodleLocation,
-            noodleDate= :noodleDate,
-            noodleTime= :noodleTime,
-            noodlePrice= :noodlePrice,
-            noodleMinTickets= :noodleMinTickets,
-            noodleMaxTickets= :noodleMaxTickets,
-            noodleTicketsSold= :noodleTicketsSold,
-            noodleCutoff= :noodleCutoff;";
+            userID= :userID, 
+            userName= :userName,
+            userFirstName= :userFirstName,
+            userLastName= :userLastName,
+            userRating= :userRating,
+            userBio= :userBio,
+            userBioLong= :userBioLong,
+            userImage= :userImage;";
+
         // prepare query
         $stmt = $this->conn->prepare($query);
 
         // sanitize
-        $this->noodleID = htmlspecialchars(strip_tags($this->noodleID));
-        $this->noodleTitle = htmlspecialchars(strip_tags($this->noodleTitle));
         $this->userID = htmlspecialchars(strip_tags($this->userID));
-        $this->noodleStatus = htmlspecialchars(strip_tags($this->noodleStatus));
-        $this->noodleDescription = htmlspecialchars(strip_tags($this->noodleDescription));
-        $this->noodleTags = htmlspecialchars(strip_tags($this->noodleTags));
-        $this->noodleImage = htmlspecialchars(strip_tags($this->noodleImage));
-        $this->noodleLocation = htmlspecialchars(strip_tags($this->noodleLocation));
-        $this->noodleDate = htmlspecialchars(strip_tags($this->noodleDate));
-        $this->noodleTime = htmlspecialchars(strip_tags($this->noodleTime));
-        $this->noodlePrice = htmlspecialchars(strip_tags($this->noodlePrice));
-        $this->noodleMinTickets = htmlspecialchars(strip_tags($this->noodleMinTickets));
-        $this->noodleMaxTickets = htmlspecialchars(strip_tags($this->noodleMaxTickets));
-        $this->noodleTicketsSold = htmlspecialchars(strip_tags($this->noodleTicketsSold));
-        $this->noodleCutoff = htmlspecialchars(strip_tags($this->noodleCutoff));
+        $this->userName = htmlspecialchars(strip_tags($this->userName));
+        $this->userFirstName = htmlspecialchars(strip_tags($this->userFirstName));
+        $this->userLastName = htmlspecialchars(strip_tags($this->userLastName));
+        $this->userRating = htmlspecialchars(strip_tags($this->userRating));
+        $this->userBio = htmlspecialchars(strip_tags($this->userBio));
+        $this->userBioLong = htmlspecialchars(strip_tags($this->userBioLong));
+        $this->userImage = htmlspecialchars(strip_tags($this->userImage));
 
         // bind values
-        $stmt->bindParam(":noodleID", $this->noodleID);
-        $stmt->bindParam(":noodleTitle", $this->noodleTitle);
         $stmt->bindParam(":userID", $this->userID);
-        $stmt->bindParam(":noodleStatus", $this->noodleStatus);
-        $stmt->bindParam(":noodleDescription", $this->noodleDescription);
-        $stmt->bindParam(":noodleTags", $this->noodleTags);
-        $stmt->bindParam(":noodleImage", $this->noodleImage);
-        $stmt->bindParam(":noodleLocation", $this->noodleLocation);
-        $stmt->bindParam(":noodleDate", $this->noodleDate);
-        $stmt->bindParam(":noodleTime", $this->noodleTime);
-        $stmt->bindParam(":noodlePrice", $this->noodlePrice);
-        $stmt->bindParam(":noodleMinTickets", $this->noodleMinTickets);
-        $stmt->bindParam(":noodleMaxTickets", $this->noodleMaxTickets);
-        $stmt->bindParam(":noodleTicketsSold", $this->noodleTicketsSold);
-        $stmt->bindParam(":noodleCutoff", $this->noodleCutoff);
-
+        $stmt->bindParam(":userName", $this->userName);
+        $stmt->bindParam(":userFirstName", $this->userFirstName);
+        $stmt->bindParam(":userLastName", $this->userLastName);
+        $stmt->bindParam(":userRating", $this->userRating);
+        $stmt->bindParam(":userBio", $this->userBio);
+        $stmt->bindParam(":userBioLong", $this->userBioLong);
+        $stmt->bindParam(":userImage", $this->userImage);
 
         // execute query
         if ($stmt->execute()) {
@@ -108,42 +87,6 @@ class User
         return false;
     }
 
-    // used when filling up the update event form
-    function readOne()
-    {
-
-        // query to read single record
-        $query = "SELECT
-                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
-            FROM
-                " . $this->table_name . " p
-                LEFT JOIN
-                    categories c
-                        ON p.category_id = c.id
-            WHERE
-                p.id = ?
-            LIMIT
-                0,1";
-
-        // prepare query statement
-        $stmt = $this->conn->prepare($query);
-
-        // bind id of event to be updated
-        $stmt->bindParam(1, $this->id);
-
-        // execute query
-        $stmt->execute();
-
-        // get retrieved row
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        // set values to object properties
-        $this->name = $row['name'];
-        $this->price = $row['price'];
-        $this->description = $row['description'];
-        $this->category_id = $row['category_id'];
-        $this->category_name = $row['category_name'];
-    }
 
     // update the event
     function update()
@@ -153,60 +96,37 @@ class User
         $query = "UPDATE
                 " . $this->table_name . "
             SET
-            noodleID= :noodleID, 
-            noodleTitle= :noodleTitle,
-            userID= :userID,
-            noodleStatus= :noodleStatus,
-            noodleDescription= :noodleDescription,
-            noodleTags= :noodleTags,
-            noodleImage= :noodleImage,
-            noodleLocation= :noodleLocation,
-            noodleDate= :noodleDate,
-            noodleTime= :noodleTime,
-            noodlePrice= :noodlePrice,
-            noodleMinTickets= :noodleMinTickets,
-            noodleMaxTickets= :noodleMaxTickets,
-            noodleTicketsSold= :noodleTicketsSold,
-            noodleCutoff= :noodleCutoff
-            WHERE
-            noodleID = :noodleID";
+            userID= :userID, 
+            userName= :userName,
+            userFirstName= :userFirstName,
+            userLastName= :userLastName,
+            userRating= :userRating,
+            userBio= :userBio,
+            userBioLong= :userBioLong,
+            userImage= :userImage;";
 
         // prepare query statement
         $stmt = $this->conn->prepare($query);
 
         // sanitize
-        $this->noodleID = htmlspecialchars(strip_tags($this->noodleID));
-        $this->noodleTitle = htmlspecialchars(strip_tags($this->noodleTitle));
         $this->userID = htmlspecialchars(strip_tags($this->userID));
-        $this->noodleStatus = htmlspecialchars(strip_tags($this->noodleStatus));
-        $this->noodleDescription = htmlspecialchars(strip_tags($this->noodleDescription));
-        $this->noodleTags = htmlspecialchars(strip_tags($this->noodleTags));
-        $this->noodleImage = htmlspecialchars(strip_tags($this->noodleImage));
-        $this->noodleLocation = htmlspecialchars(strip_tags($this->noodleLocation));
-        $this->noodleDate = htmlspecialchars(strip_tags($this->noodleDate));
-        $this->noodleTime = htmlspecialchars(strip_tags($this->noodleTime));
-        $this->noodlePrice = htmlspecialchars(strip_tags($this->noodlePrice));
-        $this->noodleMinTickets = htmlspecialchars(strip_tags($this->noodleMinTickets));
-        $this->noodleMaxTickets = htmlspecialchars(strip_tags($this->noodleMaxTickets));
-        $this->noodleTicketsSold = htmlspecialchars(strip_tags($this->noodleTicketsSold));
-        $this->noodleCutoff = htmlspecialchars(strip_tags($this->noodleCutoff));
+        $this->userName = htmlspecialchars(strip_tags($this->userName));
+        $this->userFirstName = htmlspecialchars(strip_tags($this->userFirstName));
+        $this->userLastName = htmlspecialchars(strip_tags($this->userLastName));
+        $this->userRating = htmlspecialchars(strip_tags($this->userRating));
+        $this->userBio = htmlspecialchars(strip_tags($this->userBio));
+        $this->userBioLong = htmlspecialchars(strip_tags($this->userBioLong));
+        $this->userImage = htmlspecialchars(strip_tags($this->userImage));
 
         // bind values
-        $stmt->bindParam(":noodleID", $this->noodleID);
-        $stmt->bindParam(":noodleTitle", $this->noodleTitle);
         $stmt->bindParam(":userID", $this->userID);
-        $stmt->bindParam(":noodleStatus", $this->noodleStatus);
-        $stmt->bindParam(":noodleDescription", $this->noodleDescription);
-        $stmt->bindParam(":noodleTags", $this->noodleTags);
-        $stmt->bindParam(":noodleImage", $this->noodleImage);
-        $stmt->bindParam(":noodleLocation", $this->noodleLocation);
-        $stmt->bindParam(":noodleDate", $this->noodleDate);
-        $stmt->bindParam(":noodleTime", $this->noodleTime);
-        $stmt->bindParam(":noodlePrice", $this->noodlePrice);
-        $stmt->bindParam(":noodleMinTickets", $this->noodleMinTickets);
-        $stmt->bindParam(":noodleMaxTickets", $this->noodleMaxTickets);
-        $stmt->bindParam(":noodleTicketsSold", $this->noodleTicketsSold);
-        $stmt->bindParam(":noodleCutoff", $this->noodleCutoff);
+        $stmt->bindParam(":userName", $this->userName);
+        $stmt->bindParam(":userFirstName", $this->userFirstName);
+        $stmt->bindParam(":userLastName", $this->userLastName);
+        $stmt->bindParam(":userRating", $this->userRating);
+        $stmt->bindParam(":userBio", $this->userBio);
+        $stmt->bindParam(":userBioLong", $this->userBioLong);
+        $stmt->bindParam(":userImage", $this->userImage);
 
         // execute the query
         if ($stmt->execute()) {
@@ -221,16 +141,16 @@ class User
     {
 
         // delete query
-        $query = "DELETE FROM " . $this->table_name . " WHERE noodleID = ?";
+        $query = "DELETE FROM " . $this->table_name . " WHERE userID = ?";
 
         // prepare query
         $stmt = $this->conn->prepare($query);
 
         // sanitize
-        $this->noodleID = htmlspecialchars(strip_tags($this->noodleID));
+        $this->userID = htmlspecialchars(strip_tags($this->userID));
 
         // bind id of record to delete
-        $stmt->bindParam(1, $this->noodleID);
+        $stmt->bindParam(1, $this->userID);
 
         // execute query
         if ($stmt->execute()) {
@@ -247,9 +167,7 @@ class User
         // select all query
         $query = "SELECT * FROM $this->table_name
             WHERE
-                noodleTitle LIKE ? OR noodleDescription LIKE ? OR noodleStatus LIKE ? OR noodleLocation LIKE ? OR noodleTags LIKE ?
-            ORDER BY
-                noodleID DESC";
+                userName LIKE ? OR userFirstName LIKE ? OR userLastName LIKE ?";
 
         // prepare query statement
         $stmt = $this->conn->prepare($query);
@@ -262,8 +180,6 @@ class User
         $stmt->bindParam(1, $keywords);
         $stmt->bindParam(2, $keywords);
         $stmt->bindParam(3, $keywords);
-        $stmt->bindParam(4, $keywords);
-        $stmt->bindParam(5, $keywords);
 
         // execute query
         $stmt->execute();
