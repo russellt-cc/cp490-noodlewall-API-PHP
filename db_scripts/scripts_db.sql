e1812f51807ca54a212d81e79675c11bc757adc1
griffthegrouch <69768345+griffthegrouch@users.noreply.github.com>
2021-04-19 13:48:00 -0400

    Update scripts_db.sql

--- db_scripts/scripts_db.sql
+++ db_scripts/scripts_db.sql
@@ -6,9 +6,13 @@ noodleTitle -- req
 userID -- req
 noodleStatus --req
 noodleDescription --req -- long varchar 3000 character limit
+noodleDescription
 noodleTags -- req at least one value -- currently just a long string, with comma separated values to parse through in api
-noodleImage -- string that stores address of image in website
+noodleCoverImage -- string that stores address of image in website
+noodleImages 
+noodleImageText
 noodleLocation
+noodleDirections
 noodleDate
 noodleTime
 noodlePrice -- decimal(15,2) 15 is precision(total length of value) 2 is num of digits after decimal
@@ -28,42 +32,3 @@ userRating -- float 15
 userBio
 userBioLong
 userImage -- string that stores address of image in website
-
-
-
-
-
--- create event & create user  post call templates
-
-INSERT INTO `events` (`noodleID`, `noodleTitle`, `userID`, `noodleStatus`, `noodleDescription`, `noodleTags`, `noodleImage`,
- `noodleLocation`, `noodleDate`, `noodleTime`, `noodlePrice`, `noodleMinTickets`, `noodleMaxTickets`, `noodleTicketsSold`) VALUES
-( 
-'noodleID',
-'noodleTitle',
-'userID',
-'noodleStatus',
-'noodleDescription',
-'noodleTags',
-'noodleImage',
-'noodleLocation',
-'noodleDate',
-'noodleTime',
-'noodlePrice',
-'noodleMinTickets',
-'noodleMaxTickets',
-'noodleTicketsSold',
-'noodleCutoff'
-);
-
-INSERT INTO `users` (`userID`, `userName`, `userFirstName`, `userLastName`, `userRating`, `userBio`, `userBioLong`, `userImage`) VALUES
-( 
-'userID',
-'userName',
-'userFirstName',
-'userLastName',
-'userRating',
-'userBio',
-'userBioLong',
-'userImage'
-);
-
